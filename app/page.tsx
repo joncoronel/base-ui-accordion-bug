c754bf9a631533f3bfea2e3a5c2a8bc9379cab6b
joncoronel <59675970+joncoronel@users.noreply.github.com>
2025-10-29 23:28:12 -0700

    Update dependencies and refactor page component to use Accordion from Base UI. Remove dark mode styles from globals.css and enhance page layout with new Accordion structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,119 @@
-import Image from "next/image";
+import * as React from "react";
+import { Accordion } from "@base-ui-components/react/accordion";
+import ExampleSelect from "./selecttest";
 
-export default function Home() {
+export default function ExampleAccordion() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="p-6">
+      <AccordionRoot>
+        <AccordionItem>
+          <AccordionHeader>
+            <AccordionTrigger>
+              What is Base UI?
+              <PlusIcon className="mr-2 size-3 shrink-0 transition-all ease-out group-data-[panel-open]:scale-110 group-data-[panel-open]:rotate-45" />
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionPanel>
+            <DummyContent />
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionHeader>
+            <AccordionTrigger>
+              What is Base UI?
+              <PlusIcon className="mr-2 size-3 shrink-0 transition-all ease-out group-data-[panel-open]:scale-110 group-data-[panel-open]:rotate-45" />
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionPanel>
+            <DummyContent />
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionHeader>
+            <AccordionTrigger>
+              What is Base UI?
+              <PlusIcon className="mr-2 size-3 shrink-0 transition-all ease-out group-data-[panel-open]:scale-110 group-data-[panel-open]:rotate-45" />
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionPanel>
+            <DummyContent />
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionHeader>
+            <AccordionTrigger>
+              What is Base UI?
+              <PlusIcon className="mr-2 size-3 shrink-0 transition-all ease-out group-data-[panel-open]:scale-110 group-data-[panel-open]:rotate-45" />
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionPanel>
+            <DummyContent />
+          </AccordionPanel>
+        </AccordionItem>
+      </AccordionRoot>
+      <div>hello</div>
     </div>
+  );
+}
+
+function AccordionRoot({ children }: { children: React.ReactNode }) {
+  return (
+    <Accordion.Root
+      multiple={false}
+      className={"group flex w-96 max-w-[calc(100vw-8rem)] flex-col"}
+    >
+      {children}
+    </Accordion.Root>
+  );
+}
+
+function AccordionItem({ children }: { children: React.ReactNode }) {
+  return (
+    <Accordion.Item className="relative -mt-px overflow-hidden border border-gray-300 bg-white transition-[margin,border-radius] ease-out first:rounded-t-lg last:rounded-b-lg data-[open]:mt-2 data-[open]:mb-2 data-[open]:rounded-lg data-[open]:first:mt-0 data-[open]:last:mb-0 [&:has(+_[data-open])]:rounded-b-lg [[data-open]_+_&]:rounded-t-lg">
+      {children}
+    </Accordion.Item>
+  );
+}
+
+function AccordionHeader({ children }: { children: React.ReactNode }) {
+  return <Accordion.Header>{children}</Accordion.Header>;
+}
+
+function AccordionTrigger({ children }: { children: React.ReactNode }) {
+  return (
+    <Accordion.Trigger className="group relative flex w-full items-center justify-between gap-4 bg-gray-50 py-2 pr-1 pl-3 text-left font-medium hover:bg-gray-100">
+      {children}
+    </Accordion.Trigger>
+  );
+}
+
+function AccordionPanel({ children }: { children: React.ReactNode }) {
+  return (
+    <Accordion.Panel className="h-[var(--accordion-panel-height)] overflow-hidden text-base text-gray-600 transition-[height] ease-out data-[ending-style]:h-0 data-[starting-style]:h-0">
+      {children}
+    </Accordion.Panel>
+  );
+}
+
+function DummyContent() {
+  return (
+    <div className="p-3">
+      <div className="flex items-center justify-between">
+        <span>Payment method</span>
+        <ExampleSelect />
+      </div>
+      <div className="flex items-center justify-between">
+        <span>Payment method</span>
+        <ExampleSelect />
+      </div>
+    </div>
+  );
+}
+
+function PlusIcon(props: React.ComponentProps<"svg">) {
+  return (
+    <svg viewBox="0 0 12 12" fill="currentcolor" {...props}>
+      <path d="M6.75 0H5.25V5.25H0V6.75L5.25 6.75V12H6.75V6.75L12 6.75V5.25H6.75V0Z" />
+    </svg>
   );
 }
